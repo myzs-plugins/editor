b22a19fa5ebda85af58f4378138e5326464dcb19
Kamontat Chantrachirathumrong <developer@kamontat.net>
2020-09-18 18:28:06 +0700

    Create nvim.sh

---
+++ alias/nvim.sh
@@ -0,0 +1,16 @@
+# shellcheck disable=SC1090,SC2148
+
+_myzs:internal:module:initial "$0"
+
+if _myzs:internal:checker:command-exist "nvim"; then
+  _myzs:internal:alias "v" "nvim"
+  _myzs:internal:alias-force "vi" "nvim"
+
+  _myzs:internal:alias "vs" "sudo nvim"
+  _myzs:internal:alias "vis" "sudo nvim"
+  _myzs:internal:alias "vims" "sudo nvim"
+
+  _myzs:internal:alias "sv" "sudo nvim"
+  _myzs:internal:alias "svi" "sudo nvim"
+  _myzs:internal:alias "svim" "sudo nvim"
+fi
